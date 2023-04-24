ff1dab3e14e11f222a41d0ee319c9fd86fa3c74b
Wouter Vanden Hove <wouter@libranet.eu>
2023-04-24 23:23:13 +0200

    silence poetry

--- .make/01-path.mk
+++ .make/01-path.mk
@@ -2,7 +2,8 @@
 
 # Add virtualenv to PATH, same effect as activating the virtualenv
 # VENV_BIN_DIR := $(shell pwd)/.venv/bin
-VENV_DIR := $(shell poetry env info --path)
+VENV_DIR := $(shell poetry env info --path 2> /dev/null)
+
 VENV_BIN_DIR := ${VENV_DIR}/bin
 
 export PATH := $(VENV_BIN_DIR):$(PATH)
